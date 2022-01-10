d25dae87ce2c0b1b6cba042760634e3e17426288
nysteo <teonys@nyu.edu>
2022-01-10 17:18:58 -0500

    FIX better recs

--- src/davinci.ts
+++ src/davinci.ts
@@ -6,23 +6,23 @@ import * as https from 'https';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 export class Davinci {
-    key: string = '';
-    topP: number = 1;
-    temp: number = .7;
-    maxTokens: number = 64;
-    bestOf: number = 1;
-    stop: Array<string> = ["line"];
+    key = '';
+    topP = 1;
+    temp = .13;
+    maxTokens = 64;
+    bestOf = 1;
+    stop: Array<string> = ["function", "line"];
 
 
     constructor(key: string, topP?: number, temp?: number, maxTokens?: number, stop?: Array<string>) {
         this.key = key;
-        if (topP) this.topP = topP
+        if (topP) this.topP = topP;
         if (temp) this.temp = temp;
         if (maxTokens) this.maxTokens = maxTokens;
         if (stop) this.stop = stop;
     }
 
-    async complete(text: string, language: string): Promise<Array<string>>{
+    async complete(text: string, textContext: string, language: string): Promise<Array<string>>{
         const agent = new https.Agent({
             rejectUnauthorized: false,
           });
@@ -33,9 +33,9 @@ export class Davinci {
                 'Authorization': `Bearer ${this.key}`
             },
             httpsAgent: agent,
-        }
+        };
 
-        text = `* ${language}\n*/\n ${text}`;
+        text = `${textContext}`;
 
         const suggestions: string[] | void | undefined = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', { 
             prompt: text, 
@@ -48,9 +48,10 @@ export class Davinci {
         }, config)
             .then((res: any) => res.data)
             .then((res) => {
+                vscode.window.showInformationMessage(res[0]);
                 return res?.choices.map((choice: any) => choice.text);
             })
-            .catch((err) => vscode.window.showErrorMessage(err.toString()))
+            .catch((err) => vscode.window.showErrorMessage(err.toString()));
         
         if (!suggestions) return [''];
         return suggestions as Array<string>;
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import { Davinci } from './davinci';
 export function activate(context: vscode.ExtensionContext) {
 
 	const davinci = new Davinci("");
-	let timeout: any = null;
+	const davinciOutput = vscode.window.createOutputChannel("Davinci");
 
 	const disposable = vscode.commands.registerCommand(
 		'extension.inline-completion-settings',
@@ -16,14 +16,13 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 
-	const handleGetCompletions = async( text: string, language: string): Promise<Array<string>> => {
-		vscode.window.showInformationMessage("Reteiving Completions")
-		if(!text || text.length < 6) return [''];
+	const handleGetCompletions = async( text: string, textContext: string, language: string): Promise<Array<string>> => {
+		vscode.window.showInformationMessage("Reteiving Completions");
 
-		const completions = await davinci.complete(text, language);
+		const completions = await davinci.complete(text, textContext, language);
 
 		return completions;
-	}
+	};
 
 
 	const provider: vscode.InlineCompletionItemProvider<vscode.InlineCompletionItem> = {
@@ -32,11 +31,25 @@ export function activate(context: vscode.ExtensionContext) {
 				new vscode.Range(position.with(undefined, 0), position)
 			);
 
+			let textContext = '';
+			
+			if (position.line > 1){
+				textContext = document.getText(
+					new vscode.Range(position.with(0, 0), position)
+				);
+			}
 
-			let suggestions: any = [];
 
+			davinciOutput.clear();
+			if (textContext != '') davinciOutput.appendLine("CONTEXT: \n" + textContext);
 
-			const completions = await handleGetCompletions(text, document.languageId).catch(err => vscode.window.showErrorMessage(err.toString()));
+			const suggestions: any = [];
+
+			const completions = await handleGetCompletions(
+				text,
+				textContext,
+				document.languageId
+				).catch(err => vscode.window.showErrorMessage(err.toString()));
 
 			if (!completions) return [];
 
@@ -56,5 +69,7 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.languages.registerInlineCompletionItemProvider({ pattern: "**" }, provider);
 
 	vscode.window.getInlineCompletionItemController(provider).onDidShowCompletionItem(e => {
+		davinciOutput.clear();
+		davinciOutput.appendLine("Gave Inline Reccomendation");
 	});
 }
