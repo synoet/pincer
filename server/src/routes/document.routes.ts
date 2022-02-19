823cc5837d9443f674c5ce6d86c0f9df218ebc43
synoet <teonys@nyu.edu>
2022-02-19 16:10:50 -0500

    server: add document routes

---
+++ server/src/routes/document.routes.ts
@@ -0,0 +1,54 @@
+import { Router, Request, Response } from 'express';
+
+export default (app: Router, dbClient: any) => {
+
+  /*
+    Create a new document
+  */
+    
+  app.post(
+    "/document",
+    async(req: Request, res: Response) => {
+      const {document, timeStamp, sessionId} = req.body;
+      const documents = dbClient.collection("documents");
+
+      await document
+        .insertOne({
+          sessionId: sessionId,
+          document: document,
+          timeStamp: timeStamp
+        }, (err: any) => {
+          if(err){
+            res.status(500).send({message: "failed to add document"})
+            return;
+          }
+        })
+
+      res.status(201).send({message: "Success!"})
+    }
+  );
+
+  /*
+    Get timestamp of last document
+  */
+  app.get(
+    "/document/:sessionId/last",
+    async (req: Request, res: Response): Promise<any> => {
+      const {sessionId} = req.params;
+
+      const documents = dbClient.collection("documents");
+
+      const lastDocument = await documents
+        .find({sessionId: sessionId})
+        .sort({timeStamp: -1})
+        .limit(1)
+        .toArray();
+
+      if (!lastDocument[0]) {
+        res.status(200).json(null);
+        return;
+      }
+      res.status(200).json(lastDocument[0]);
+    }
+  );
+}
