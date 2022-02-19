274fcfa34e13e29dce5ea11fc890b75653f70d11
synoet <teonys@nyu.edu>
2022-02-19 15:52:06 -0500

    server: comment and cleanup session routes

--- server/src/routes/session.routes.ts
+++ server/src/routes/session.routes.ts
@@ -1,6 +1,57 @@
 import { Router, Request, Response } from 'express';
 
 export default (app: Router, dbClient: any) => {
+  /*
+    Create a new session for a user, with an id.
+  */
+  app.post(
+    "/session",
+   async (req: Request, res: Response): Promise<any> => {
+      const {userId, sessionId} = req.body;
+
+      const users = dbClient.collection("users");
+
+      const user = await users.find({userId: userId}).limit(1).toArray();
+
+      if (!user[0]) return;
+
+      await users
+        .updateOne({userId: userId}, {
+          $set: {
+            userId: userId,
+            activated: user[0].activated || true,
+            sessions: user[0].sessions.length > 0 ? [... user[0].sessions, sessionId] : [sessionId],
+          }
+        }, (err: any, res:any) => {
+            if(err) throw err 
+            if (res) console.log(res);
+        });
+
+      res.status(201).send({message: 'Success'});
+    }
+  );
+
+  /*
+    Get all recent sessions
+  */
+  app.get(
+    "/session",
+    async (req: Request, res: Response) => {
+      const sessions = dbClient.collection("sessions");
+
+      sessions
+        .find({})
+        .sort({latestPing: -1})
+        .toArray((err: any, result: any) => {
+          if (err) console.log(err);
+          res.json(result);
+        })
+    }
+  );
+
+  /*
+    Get session info by its id.
+  */
   app.get(
     "/session/:sessionId",
     async (req: Request, res: Response) => {
@@ -19,22 +70,10 @@ export default (app: Router, dbClient: any) => {
     }
   );
 
-  app.get(
-    "/session",
-    async (req: Request, res: Response) => {
-      const sessions = dbClient.collection("sessions");
 
-      sessions
-        .find({})
-        .limit(12)
-        .sort({latestPing: -1})
-        .toArray((err: any, result: any) => {
-          if (err) console.log(err);
-          res.json(result);
-        })
-    }
-  );
-
+    /*
+    Get a session by a user id
+  */
   app.get(
     "/session/user/:userId",
     async (req: Request, res: Response) => {
@@ -69,4 +108,44 @@ export default (app: Router, dbClient: any) => {
     }
   );
 
+
+
+  /*
+    Extension will ping a session to keep it alive
+  */
+  app.post(
+    "/session/ping",
+    async (req: Request, res: Response) => {
+      const {sessionId} = req.body;
+
+      const sessions = await dbClient.collection("sessions");
+
+      const session = await sessions
+                        .find({sessionId: sessionId})
+                        .limit(1)
+                        .toArray();
+
+      if(session.length === 0){
+        sessions
+          .insertOne({          
+            sessionId: req.body.sessionId,
+            startTime: new Date(),
+            latestPing: new Date(),
+          })
+
+        return res.status(201).send();
+      }
+
+      sessions.updateOne({sessionId: sessionId}, {
+        $set: {
+          ...session[0],
+          latestPing: new Date(),
+        }
+      }, (err: any) => {
+          if (err) console.log(err);
+      });
+
+      return res.status(201).send();
+    }
+  );
 }
