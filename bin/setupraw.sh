520135c37cebb1f26f1b404d7f0b669f8a7f6e28
synoet <teonys@nyu.edu>
2022-03-01 14:55:56 -0500

    ext: update raw script

--- bin/setupraw.sh
+++ bin/setupraw.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 code-insiders --install-extension davinci.vsix
-code-insiders --no-sandbox --enable-proposed-api nysteo.davinci
+code-insiders --no-sandbox --enable-proposed-api --user-data-dir=/home/anubis nysteo.davinci
 
 
