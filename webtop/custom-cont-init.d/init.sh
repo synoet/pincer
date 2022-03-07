65dc9b2af5e0b0f25aeb31ab600ccffe86608f07
synoet <teonys@nyu.edu>
2022-03-07 18:22:45 -0500

    webtop: upadate dockerfile

--- webtop/custom-cont-init.d/init.sh
+++ webtop/custom-cont-init.d/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pushd /var/lib/pincer/bin
+pushd /var/lib/Pincer/bin
 chmod +x ./setupraw.sh
 su abc -c './setupraw.sh'
 popd
 
 mkdir /home/anubis/.config/autostart
 
-mv /etc/custom-cont-init.d/code.desktop /home/anubis/.config/autostart/
+mv /home/anubis/custom-cont-init.d/code.desktop /home/anubis/.config/autostart/
