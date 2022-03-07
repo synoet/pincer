cd94f081ad2a49aa6bc6ceda8788a492b84f12e1
synoet <teonys@nyu.edu>
2022-03-06 21:56:13 -0500

    webtop: fixup

--- webtop/custom-cont-init.d/init.sh
+++ webtop/custom-cont-init.d/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pushd /home/anubis/Davinci/bin
+pushd /var/lib/pincer/bin
 chmod +x ./setupraw.sh
 su abc -c './setupraw.sh'
 popd
 
 mkdir /home/anubis/.config/autostart
 
-mv /home/anubis/custom-cont-init.d/code.desktop /home/anubis/.config/autostart/
+mv /etc/custom-cont-init.d/code.desktop /home/anubis/.config/autostart/
