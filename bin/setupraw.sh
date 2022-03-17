885753608206995d9b341d745a75716305b6721d
synoet <teonys@nyu.edu>
2022-03-17 19:44:32 -0400

    extension: add c extension

--- bin/setupraw.sh
+++ bin/setupraw.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 code-insiders --install-extension pincer.vsix
+code-insiders --install-extension ms-vscode.cpptools
 code-insiders --no-sandbox --enable-proposed-api synoet.pincer
 
 
