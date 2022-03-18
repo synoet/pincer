30b46d49f97a3870b38726138926331ed5b46e43
synoet <teonys@nyu.edu>
2022-03-18 16:20:35 -0400

    webtop: update

--- webtop/custom-cont-init.d/init.sh
+++ webtop/custom-cont-init.d/init.sh
@@ -5,6 +5,18 @@ chmod +x ./setupraw.sh
 su abc -c './setupraw.sh'
 popd
 
+mv /var/lib/content.zip /home/anubis/content.zip
+pushd /home/anubis
+mkdir study_content
+mv content.zip ./study_content/
+popd
+
+pushd /home/anubis/study_content
+unzip content.zip
+rm -rf content.zip
+popd
+
 mkdir /home/anubis/.config/autostart
 
 mv /home/anubis/custom-cont-init.d/code.desktop /home/anubis/.config/autostart/
+
