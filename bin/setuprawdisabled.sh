bbbee4e918671039a144915898500d77c5ad8898
synoet <teonys@nyu.edu>
2022-03-20 13:48:08 -0400

    webtop: disabled vers

---
+++ bin/setuprawdisabled.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+code-insiders --install-extension pincer_disabled.vsix
+code-insiders --install-extension ms-vscode.cpptools
+code-insiders --no-sandbox --enable-proposed-api synoet.pincer
+
+
---
+++ webtop/webtop-pincer-disabled/custom-cont-init.d/init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ ! -f /home/anubis/.davinci.conf ]]
+then
+
+  # install extension from .vsix file
+  pushd /var/lib/Pincer/bin
+  chmod +x ./setuprawdisabled.sh
+  su abc -c './setupraw.sh'
+  popd
+fi
+
+if [[ ! -d /home/anubis/study_content ]]
+then
+
+  # copy over contents of the study
+  mv /var/lib/content.zip /home/anubis/content.zip
+  pushd /home/anubis
+  mkdir study_content
+  mv content.zip ./study_content/
+  popd
+
+  pushd /home/anubis/study_content
+  unzip content.zip
+  rm -rf content.zip
+  popd
+
+  chown -c -R abc:abc /home/anubis/study_content
+fi
+
+if [[ ! -d /home/anubis/.config/autostart ]]
+then
+
+  # create start up scripts for xfce
+  mkdir /home/anubis/.config/autostart
+  mv /home/anubis/custom-cont-init.d/code.desktop /home/anubis/.config/autostart/
+fi
+
